0d959b19ccbd77109cc32bccb510fc275ae2c2c6
João Sá <up201506252@fe.up.pt>
2018-04-23 01:54:19 +0100

    :construction:

--- core/src/com/battleship/Battleship.java
+++ core/src/com/battleship/Battleship.java
@@ -1,33 +1,45 @@
 package com.battleship;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Battleship extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Battleship extends Game {
+	private SpriteBatch batch;
+	private AssetManager assetManager;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		assetManager = new AssetManager();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		assetManager.dispose();
 	}
+
+    /**
+     * Returns the asset manager used to load all textures and sounds.
+     *
+     * @return the asset manager
+     */
+    public AssetManager getAssetManager() {
+        return assetManager;
+    }
+
+    /**
+     * Returns the sprite batch used to improve drawing performance.
+     *
+     * @return the sprite batch
+     */
+    public SpriteBatch getBatch() {
+        return batch;
+    }
 }
