b6c1aa9ee80b4bdf2997deeda60b95d2da5335c9
Bernardo Barbosa <up201503477@fe.up.pt>
2018-05-05 15:44:43 +0100

    improved main menu view :gem:

--- core/src/com/battleship/view/MainMenuView.java
+++ core/src/com/battleship/view/MainMenuView.java
@@ -3,6 +3,7 @@ package com.battleship.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -33,6 +34,9 @@ public class MainMenuView extends ScreenAdapter {
     private TextButton multiplayerBtn;
     private TextButton exitBtn;
 
+    private Texture background;
+    private Texture title;
+
     /**
      * Creates this screen.
      */
@@ -43,6 +47,9 @@ public class MainMenuView extends ScreenAdapter {
         this.stage.clear();
         Gdx.input.setInputProcessor(stage);
 
+        this.background = new Texture("background.png");
+        this.title = new Texture("battleship.png");
+
         this.skin = new Skin(Gdx.files.internal("skin/quantum-horizon-ui.json"));
         this.table = new Table();
 
@@ -52,20 +59,28 @@ public class MainMenuView extends ScreenAdapter {
 
     private void setUpButtons(){
         singleplayerBtn  = new TextButton("SinglePlayer", skin, "default");
+        singleplayerBtn.setColor(0,0,1,1);
+        singleplayerBtn.getLabel().setFontScale(2);
         singleplayerBtn.addListener( new ClickListener(){
             @Override
             public void clicked(InputEvent e, float x, float y){
                 game.startSingleplayer();
             }
         });
+
         multiplayerBtn = new TextButton("Multiplayer", skin, "default");
+        multiplayerBtn.setColor(0,0,1,1);
+        multiplayerBtn.getLabel().setFontScale(2);
         multiplayerBtn.addListener( new ClickListener(){
             @Override
             public void clicked(InputEvent e, float x, float y){
                 game.startMultiplayer();
             }
         });
+
         exitBtn = new TextButton("Exit", skin, "default");
+        exitBtn.setColor(0,0,1,1);
+        exitBtn.getLabel().setFontScale(2);
         exitBtn.addListener( new ClickListener(){
             @Override
             public void clicked(InputEvent e, float x, float y){
@@ -80,13 +95,13 @@ public class MainMenuView extends ScreenAdapter {
         stage.addActor(table);
 
         table.row();
-        table.add(singleplayerBtn);
+        table.add(singleplayerBtn).width(Gdx.graphics.getWidth()/2).height(100);
 
         table.row();
-        table.add(multiplayerBtn);
+        table.add(multiplayerBtn).width(Gdx.graphics.getWidth()/2).height(100);
 
         table.row();
-        table.add(exitBtn);
+        table.add(exitBtn).width(Gdx.graphics.getWidth()/2).height(100);
     }
 
     /**
@@ -101,6 +116,11 @@ public class MainMenuView extends ScreenAdapter {
 
         Gdx.gl.glClearColor( 0, 0, 1f, 1 );
         Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+
+        game.getBatch().begin();
+        game.getBatch().draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        game.getBatch().draw(title,200,1200, 700, 400);
+        game.getBatch().end();
 
 //        stage.act();
         stage.draw();
