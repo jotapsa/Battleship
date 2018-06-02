8bba0533e3f60da86b06613e1ada880e3dcef3bc
Bernardo Barbosa <up201503477@fe.up.pt>
2018-06-02 22:13:04 +0100

    start Tests :hammer:

---
+++ core/src/test/TestGameModel.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.battleship.controller.GameController;
+import com.battleship.model.GameModel;
+import com.battleship.model.GameType;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestGameModel {
+
+    private GameModel gameModel;
+    private GameController gameController;
+
+    @Test
+    public void testGameModel(){
+        gameModel = new GameModel(GameType.SinglePlayer);
+        gameController = new GameController(gameModel);
+
+        assertEquals(gameModel.getGameType(), GameType.SinglePlayer);
+    }
+
+
+}
